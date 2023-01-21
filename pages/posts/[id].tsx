268a9ea5199a1255708bf768c154cb43e12a9bb5
Iva Opanasiuk <vanjoker88@gmail.com>
2023-01-21 16:54:13 +0300

    go to ts

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,10 +1,21 @@
+import {GetStaticPaths, GetStaticProps} from 'next';
+import Head from 'next/head';
+
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
 import Date from '../../components/date'
+
 import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({ postData }) {
+type Props = {
+    postData: {
+        title: string;
+        date: string;
+        contentHtml: string
+    }
+}
+
+export default function Post({ postData } : Props ) {
     return (
         <Layout>
             <Head>
@@ -21,7 +32,7 @@ export default function Post({ postData }) {
     )
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
     const paths = getAllPostIds()
     return {
         paths,
@@ -29,8 +40,8 @@ export async function getStaticPaths() {
     }
 }
 
-export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const postData = await getPostData(params?.id as string)
     return {
         props: {
             postData
